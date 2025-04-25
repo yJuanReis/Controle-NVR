ac36b35d7c1f1c652d18afe5d2357ee9b3d0cb99
Juan <juanreissilva@gmail.com>
2025-04-25 15:30:07 -0300

    Corrigir problema de tela branca mudando para BrowserRouter com basename correto

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import { NVRProvider } from "./context/NVRContext";
 import NVRList from "./pages/NVRList";
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
+        <BrowserRouter basename="/Controle-NVR">
           <Layout>
             <Routes>
               <Route path="/" element={<NVRList />} />
@@ -29,7 +29,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </Layout>
-        </HashRouter>
+        </BrowserRouter>
       </TooltipProvider>
     </NVRProvider>
   </QueryClientProvider>
